abbc853721681e03049c20fff776987115838430
ne <ne@n3k.de>
2023-09-06 10:29:12 +0200

    Added folder for postgres

--- backend/core/models/database.py
+++ backend/core/models/database.py
@@ -1,17 +1,42 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import create_engine, Column, String, Boolean, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+import datetime
 
 Base = declarative_base()
 
 class User(Base):
-    """
-    Database Model for the User.
-    """
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = Column(String, primary_key=True, index=True)
-    first_name = Column(String, unique=True, index=True, nullable=False)
-    last_name = Column(String, unique=False, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, unique=True, index=True, nullable=False)
-    is_privileged = Column(Boolean, default=False, unqiue=False, index=True, nullable=False)
+    id = Column(UUID, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False, comment="Hashed")
+    is_privileged = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    last_login = Column(DateTime)
+
+    # Relationship with passwords table
+    passwords = relationship("Password", back_populates="user")
+
+class Password(Base):
+    __tablename__ = 'passwords'
+
+    id = Column(UUID, primary_key=True)
+    content = Column(String, nullable=False, comment="Encrypted")
+    user_id = Column(UUID, ForeignKey('users.id'), nullable=False)
+    title = Column(String, nullable=False)
+    username = Column(String, nullable=True)
+    url = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+    # Relationship with users table
+    user = relationship("User", back_populates="passwords")
+
+# Create an engine and bind it to the Base
+engine = create_engine('postgresql://username:password@localhost:5432/mydatabase')
+Base.metadata.create_all(engine)
